d8b9e9bdae683bab1d4cc668a48c78871781b07a
Mary <maryndungu1412@gmail.com>
2025-07-13 16:56:44 +0300

    components folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
