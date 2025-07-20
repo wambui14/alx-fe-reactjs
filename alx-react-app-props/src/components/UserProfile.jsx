268f69c56b153e8c12c687dd970010eabdef4286
Mary <maryndungu1412@gmail.com>
2025-07-20 20:43:40 +0300

    use of context

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
- const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
- 
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Full Name: {userData.name}</p>
+      <p>Email Address: {userData.email}</p>
+    </div>
+  );
+}
+
 export default UserProfile;
